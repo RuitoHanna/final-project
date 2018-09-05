6b9037c3563e7b1088f6b184ce9f130dbb141251
RuitoHanna <41068522+RuitoHanna@users.noreply.github.com>
2018-09-05 15:34:54 +0700

    Add files via upload

---
+++ js/kucing_terbang_studio.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(window).on("load", function() {
+    preloaderSlideupTime = 3000;
+    function hidePreloader() {
+    var preloader = $('.wrapper-dark');
+    preloader.slideUp(preloaderSlideupTime);
+    }
+    hidePreloader();
+    });
+    });
+    $(document).ready(function() {
+        $('#media').carousel({
+          pause: true,
+          interval: false,
+        });
+      });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
